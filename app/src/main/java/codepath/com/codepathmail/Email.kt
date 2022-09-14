b638c7e08239f348beadd63ffe679f847f0ad322
xingh <nevinzheng0921@gmail.com>
2022-09-13 23:30:58 -0400

    Commit files

---
+++ app/src/main/java/codepath/com/codepathmail/Email.kt
@@ -0,0 +1,7 @@
+package codepath.com.codepathmail
+
+import org.w3c.dom.DOMStringList
+
+class Email(val sender:String,val title:String,val summary:String)
+
+// a model that defines the infomation contained in each Email
---
+++ app/src/main/java/codepath/com/codepathmail/EmailAdapter.kt
@@ -0,0 +1,52 @@
+package codepath.com.codepathmail
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+
+class EmailAdapter(private val emails: List<Email>): RecyclerView.Adapter<EmailAdapter.ViewHolder>()
+{
+    // Provide a direct reference to each of the views within a data item
+    // Used to cache the views within the item layout for fast access
+    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView)
+    {
+        val senderTextView : TextView
+        val titleTextView : TextView
+        val summaryTextView : TextView
+
+        init
+        {
+            senderTextView = itemView.findViewById<TextView>(R.id.senderTv)
+            titleTextView = itemView.findViewById<TextView>(R.id.titleTv)
+            summaryTextView = itemView.findViewById<TextView>(R.id.summaryTv)
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder
+    {
+        val context = parent.context
+        val inflater = LayoutInflater.from(context)
+        // Inflate the custom layout
+        val contactView = inflater.inflate(R.layout.email_item, parent, false)
+        // Return a new holder instance
+        return ViewHolder(contactView)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int)
+    {
+        // Get the data model based on position
+        val email = emails.get(position)
+        // Set item views based on views and data model
+        holder.senderTextView.text = email.sender
+        holder.titleTextView.text = email.title
+        holder.summaryTextView.text = email.summary
+    }
+
+    override fun getItemCount(): Int
+    {
+        return emails.size
+    }
+
+}
